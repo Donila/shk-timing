3ab276427b2f0c097e89265d9868511423f87afc
Daniil Dziaruhin <daniil.dziaruhin@pointsource.com>
2019-03-29 00:49:59 +0300

    Added mobile navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <template>
   <div>
     <v-app dark>
+      <v-navigation-drawer v-model="drawer" fixed right app>
+        <v-list dense>
+          <v-list-tile @click="home()">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ $t('newAttack') }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <Languages/>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>info</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <Help/>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
       <v-toolbar fixed app>
+        <v-toolbar-side-icon class="hidden-lg-and-up" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
         <v-toolbar-title class="hidden-md-and-down">{{ $t("title") }}</v-toolbar-title>
         <v-toolbar-title class="hidden-lg-and-up">{{ $t("titleShort") }}</v-toolbar-title>
         <v-spacer></v-spacer>
@@ -32,6 +61,7 @@ export default {
   },
   data() {
     return {
+      drawer: false,
       helpDialog: false,
       store: this.$root.$data
     };
