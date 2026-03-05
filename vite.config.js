45d502931995b80018dd3b46f6e76390777dd9bf
dd <donila2008@gmail.com>
2026-03-05 22:40:51 +0300

    add prerender

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import vitePrerender from 'vite-plugin-prerender'
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
+    vitePrerender({
+      staticDir: path.join(path.dirname(fileURLToPath(import.meta.url)), 'dist'),
+      routes: ['/', '/about'],
+    }),
   ],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
